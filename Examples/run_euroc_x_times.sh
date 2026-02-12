aa5c25e5f518b372cdd68a98d255f5b8557c45f0
soudabemhashemi <s.mhashemi.221@gmail.com>
2026-02-12 13:45:29 -0800

    Fix run euroc x times

--- Examples/run_euroc_x_times.sh
+++ Examples/run_euroc_x_times.sh
@@ -7,17 +7,17 @@
 #      ./euroc_eval_examples.sh 0 0 0 V101 ../Results/ >> ./test/terminal_log.txt 2>&1
 # done
 
-# echo "Run V102" >> ./test/timing.txt
-# for i in {1..10}
-# do
-#     echo "Run $i/10"
-#      ./euroc_eval_examples.sh 0 0 0 V102 ../Results/ >> ./test/terminal_log.txt 2>&1
-# done
+echo "Run V102" >> ./test/timing.txt
+for i in {1..20}
+do
+    echo "Run $i/20"
+     ./euroc_eval_examples.sh 0 0 0 V102 ../Results/ >> ./test/terminal_log.txt 2>&1
+done
 
 echo "Run V103" >> ./test/timing.txt
-for i in {1..10}
+for i in {1..20}
 do
-    echo "Run $i/10"
+    echo "Run $i/20"
      ./euroc_eval_examples.sh 0 0 0 V103 ../Results/ >> ./test/terminal_log.txt 2>&1
 done
 
@@ -28,16 +28,16 @@ done
 #      ./euroc_eval_examples.sh 0 0 0 V201 ../Results/ >> ./test/terminal_log.txt 2>&1
 # done
 
-# echo "Run V202" >> ./test/timing.txt
-# for i in {1..10}
-# do
-#     echo "Run $i/10"
-#      ./euroc_eval_examples.sh 0 0 0 V202 ../Results/ >> ./test/terminal_log.txt 2>&1
-# done
+echo "Run V202" >> ./test/timing.txt
+for i in {1..20}
+do
+    echo "Run $i/20"
+     ./euroc_eval_examples.sh 0 0 0 V202 ../Results/ >> ./test/terminal_log.txt 2>&1
+done
 
-# echo "Run V203" >> ./test/timing.txt
-# for i in {1..10}
-# do
-#     echo "Run $i/10"
-#      ./euroc_eval_examples.sh 0 0 0 V203 ../Results/ >> ./test/terminal_log.txt 2>&1
-# done
+echo "Run V203" >> ./test/timing.txt
+for i in {1..20}
+do
+    echo "Run $i/20"
+     ./euroc_eval_examples.sh 0 0 0 V203 ../Results/ >> ./test/terminal_log.txt 2>&1
+done
