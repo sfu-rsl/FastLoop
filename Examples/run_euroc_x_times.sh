9a70aee729599d71b61a52acf0a85856e56e7b64
soudabemhashemi <s.mhashemi.221@gmail.com>
2026-02-13 12:43:16 -0800

    Modify the script

--- Examples/run_euroc_x_times.sh
+++ Examples/run_euroc_x_times.sh
@@ -7,19 +7,19 @@
 #      ./euroc_eval_examples.sh 0 0 0 V101 ../Results/ >> ./test/terminal_log.txt 2>&1
 # done
 
-# echo "Run V102" >> ./test/timing.txt
-# for i in {1..10}
-# do
-#     echo "Run $i/10"
-#      ./euroc_eval_examples.sh 0 0 0 V102 ../Results/ >> ./test/terminal_log.txt 2>&1
-# done
+echo "Run V102" >> ./test/timing.txt
+for i in {1..10}
+do
+    echo "Run $i/10"
+     ./euroc_eval_examples.sh 0 0 0 V102 ../Results/ >> ./test/terminal_log.txt 2>&1
+done
 
-# echo "Run V103" >> ./test/timing.txt
-# for i in {1..10}
-# do
-#     echo "Run $i/10"
-#      ./euroc_eval_examples.sh 0 0 0 V103 ../Results/ >> ./test/terminal_log.txt 2>&1
-# done
+echo "Run V103" >> ./test/timing.txt
+for i in {1..10}
+do
+    echo "Run $i/10"
+     ./euroc_eval_examples.sh 0 0 0 V103 ../Results/ >> ./test/terminal_log.txt 2>&1
+done
 
 echo "Run V201" >> ./test/timing.txt
 for i in {1..10}
@@ -35,9 +35,9 @@ do
      ./euroc_eval_examples.sh 0 0 0 V202 ../Results/ >> ./test/terminal_log.txt 2>&1
 done
 
-echo "Run V203" >> ./test/timing.txt
-for i in {1..10}
-do
-    echo "Run $i/10"
-     ./euroc_eval_examples.sh 0 0 0 V203 ../Results/ >> ./test/terminal_log.txt 2>&1
-done
+# echo "Run V203" >> ./test/timing.txt
+# for i in {1..10}
+# do
+#     echo "Run $i/10"
+#      ./euroc_eval_examples.sh 0 0 0 V203 ../Results/ >> ./test/terminal_log.txt 2>&1
+# done
