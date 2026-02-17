20c44c1d0985eee34b24e70ceb4754ed60c9f88b
soudabemhashemi <s.mhashemi.221@gmail.com>
2026-02-17 13:18:02 -0800

    Add V203 to the script

--- Examples/run_euroc_x_times.sh
+++ Examples/run_euroc_x_times.sh
@@ -8,9 +8,9 @@
 # done
 
 echo "Run V102" >> ./test/timing.txt
-for i in {1..10}
+for i in {1..5}
 do
-    echo "Run $i/10"
+    echo "Run $i/5"
      ./euroc_eval_examples.sh 0 0 0 V102 ../Results/ >> ./test/terminal_log.txt 2>&1
 done
 
@@ -21,23 +21,23 @@ do
      ./euroc_eval_examples.sh 0 0 0 V103 ../Results/ >> ./test/terminal_log.txt 2>&1
 done
 
-echo "Run V201" >> ./test/timing.txt
-for i in {1..10}
-do
-    echo "Run $i/10"
-     ./euroc_eval_examples.sh 0 0 0 V201 ../Results/ >> ./test/terminal_log.txt 2>&1
-done
-
-echo "Run V202" >> ./test/timing.txt
-for i in {1..10}
-do
-    echo "Run $i/10"
-     ./euroc_eval_examples.sh 0 0 0 V202 ../Results/ >> ./test/terminal_log.txt 2>&1
-done
-
-# echo "Run V203" >> ./test/timing.txt
+# echo "Run V201" >> ./test/timing.txt
 # for i in {1..10}
 # do
 #     echo "Run $i/10"
-#      ./euroc_eval_examples.sh 0 0 0 V203 ../Results/ >> ./test/terminal_log.txt 2>&1
+#      ./euroc_eval_examples.sh 0 0 0 V201 ../Results/ >> ./test/terminal_log.txt 2>&1
 # done
+
+# echo "Run V202" >> ./test/timing.txt
+# for i in {1..10}
+# do
+#     echo "Run $i/10"
+#      ./euroc_eval_examples.sh 0 0 0 V202 ../Results/ >> ./test/terminal_log.txt 2>&1
+# done
+
+echo "Run V203" >> ./test/timing.txt
+for i in {1..10}
+do
+    echo "Run $i/10"
+     ./euroc_eval_examples.sh 0 0 0 V203 ../Results/ >> ./test/terminal_log.txt 2>&1
+done
